368d929193b7c6f961a14cbc9c871ff0339f3e15
Yuki Kitayama <ykitayama@corp.guzman.com>
2021-07-10 21:17:30 -0600

    still week3

---
+++ basic/week3/script22.js
@@ -0,0 +1,10 @@
+(function(){
+  "use strict";
+  
+  const pTag = document.querySelector('p');
+  const btn = document.querySelector('button');
+  btn.addEventListener('click', function(){
+    pTag.style.color = "green";
+  });
+
+}());
