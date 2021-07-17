7a92bb365d0c968c1d61de80094dcbed4a22d6a5
Yuki Kitayama <yk2797@columbia.edu>
2021-07-16 21:13:03 -0600

    week2 in animation

---
+++ animation/week1/WEB1091_jQuery-folding-menus-Start/script.js
@@ -0,0 +1,12 @@
+(function(){
+
+  "use strict";
+
+  $('ul li ul').hide();
+  $('.menulink').click(function(){
+    const thisMenu = $(this).next('ul');
+    $('ul li ul').not(thisMenu).hide();
+    thisMenu.toggle();
+  });	
+
+})();
