576c59c2d93a6ddb4012171f99815df1a29e8b9b
Yuki Kitayama <yk2797@columbia.edu>
2021-07-20 21:23:03 -0600

    started week3

---
+++ animation/week3/WEB1091_Slider-V1-Start/script.js
@@ -0,0 +1,43 @@
+// Add your JS here
+$(window).on('load', function(){
+
+  'use strict';
+
+  const imageCount = $("#slider ul li").length;
+  const imageWidth = $("#slider ul li img").first().width();
+  const totalWidth = (imageWidth * imageCount) + 'px';
+  // alert(totalWidth);
+
+  let leftPosition = 0;
+  let counter = 0;
+
+  $('#slider ul').css('width', totalWidth);
+
+  $('#next').click(function(){
+    counter++;
+
+    if (counter === imageCount) {
+      counter = 0;
+    }
+
+    leftPosition = `-${counter * imageWidth}px`;
+    // console.log(leftPosition);
+
+    // 700 milliseconds
+    $('#slider ul').animate({left: leftPosition}, 700, "easeInQuad");
+  });
+
+  $('#previous').click(function(){
+
+    counter--;
+
+    if (counter < 0) {
+      counter = imageCount - 1;
+    }
+
+    leftPosition = `-${counter * imageWidth}px`;
+    $('#slider ul').animate({left: leftPosition}, 700, "easeInQuad");
+
+  });
+});
+
