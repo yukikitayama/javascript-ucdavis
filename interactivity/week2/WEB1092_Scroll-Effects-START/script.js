047d775bf2f677a4c9570f943a73a76bd177b48d
Yuki Kitayama <yk2797@columbia.edu>
2021-08-02 21:36:51 -0600

    finished week3

--- interactivity/week2/WEB1092_Scroll-Effects-START/script.js
+++ interactivity/week2/WEB1092_Scroll-Effects-START/script.js
@@ -9,7 +9,7 @@ function smoothScroll(event){
   const targetID = event.target.getAttribute('href');
   const targetSection = document.querySelector(targetID);
   const originalTop = Math.floor(targetSection.getBoundingClientRect().top) - 200;
-  console.log(originalTop);
+  // console.log(originalTop);
   window.scrollBy({top: originalTop, left: 0, behavior: 'smooth'});
 }
 
@@ -20,5 +20,66 @@ window.addEventListener('load', function(){
   let counter = 1;
   let prevCounter = 1;
   let doneResizing;
-  console.log(posts[0].getBoundingClientRect().top);
+  // console.log(posts[0].getBoundingClientRect().top + window.pageYOffset);
+
+  // posts.forEach(function(post){
+  //   postTops.push(Math.floor(post.getBoundingClientRect().top + window.pageYOffset));
+  // });
+  resetPagePosition();
+  // console.log(postTops);
+
+  this.window.addEventListener('scroll', function(){
+    pagetop = window.pageYOffset + 250;
+    // console.log(pagetop) + 250;
+
+    if (pagetop > postTops[counter]) {
+      counter++;
+      console.log(`scrolling down ${counter}`);
+    } else if (counter > 1 && pagetop < postTops[counter - 1]) {
+      counter--;
+      console.log(`scrolling up ${counter}`);
+    }
+
+    if (counter != prevCounter) {
+      navLinks.forEach(function(eachLink){
+        eachLink.removeAttribute('class');
+      });
+
+      const thisLink = document.querySelector(`nav ul li:nth-child(${counter}) a`);
+      thisLink.className = 'selected';
+      prevCounter = counter;
+    }
+  });
+
+  window.addEventListener('resize', function(){
+    clearTimeout(doneResizing);
+    doneResizing = setTimeout(function(){
+      // console.log('done resizing!');
+      resetPagePosition();
+    }, 500);
+  });
+
+  function resetPagePosition(){
+    postTops = [];
+
+    posts.forEach(function(post){
+      postTops.push(Math.floor(post.getBoundingClientRect().top + window.pageYOffset));
+    });
+
+    const pagePosition = window.pageYOffset + 250;
+    counter = 0;
+
+    postTops.forEach(function(post){
+      if (pagePosition > post) {
+        counter++;
+      }
+    });
+
+    navLinks.forEach(function(eachLink){
+      eachLink.removeAttribute('class');
+    });
+
+    const thisLink = document.querySelector(`nav ul li:nth-child(${counter}) a`);
+    thisLink.className = 'selected';
+  }
 });
